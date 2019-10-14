375785e385ff7146cc15427be4a191f9a748a454
Jas Spencer <jas.spencer414@gmail.com>
2019-10-14 13:54:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+deli << name
+puts "Welcome, #{name}. You are #{deli.length} in line."
+end
